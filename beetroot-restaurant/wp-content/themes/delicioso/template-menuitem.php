e44ccca46c24580f881028524bc386b173dce8a3
saldesign <saldarriagadesign@gmail.com>
2016-05-06 11:54:35 -0700

    somemo css

--- beetroot-restaurant/wp-content/themes/delicioso/template-menuitem.php
+++ beetroot-restaurant/wp-content/themes/delicioso/template-menuitem.php
@@ -40,37 +40,45 @@ $taxonomy = 'menuitemcat'; ?>
 <?php endif;  //end THE LOOP ?>
 
 <main class="content">
-<?php         
-// Gets every term in this taxonomy
-$terms = get_terms( $taxonomy );
+<div class="main container">
+    <?php         
+    // Gets every term in this taxonomy
+    $terms = get_terms( $taxonomy );
 
-//go through each term in this taxonomy one at a time
-foreach( $terms as $term ) : 
+    //go through each term in this taxonomy one at a time
+    foreach( $terms as $term ) : 
 
-    //get ONE post assigned to this term
-    $custom_loop = new WP_Query( array(
-        'post_type' => $post_type,
-        'taxonomy' => $taxonomy,
-        'term' => $term->slug,
-        ) );
-    
-    //LOOP
-    if( $custom_loop->have_posts() ): ?>
-    <h2><?php echo $term->name; ?></h2>
-    <?php
-        while( $custom_loop->have_posts() ) : $custom_loop->the_post(); ?>
-<article class="cf">
-    <div>
-        <h3><?php the_title(); ?></h3>
-        <?php the_content( ); ?>
-    </div>
-    <?php the_post_thumbnail(); ?>
-</article>
+        //get ONE post assigned to this term
+        $custom_loop = new WP_Query( array(
+            'post_type' => $post_type,
+            'taxonomy' => $taxonomy,
+            'term' => $term->slug,
+            ) );
+        
+        //LOOP
+        if( $custom_loop->have_posts() ): ?>
+        <h2><?php echo $term->name; ?></h2>
+        <?php
+            while( $custom_loop->have_posts() ) : $custom_loop->the_post(); ?>
+    <article class="cf">
+        <div>
+            <h3><?php the_title(); ?></h3>
+            <?php the_content( ); ?>
+            <?php //get the 'Price' custom field
+            //                      post ID , field name , single?
+            $price = get_post_meta( $post->ID, 'Price', true );
+            if($price){ ?>
+            <span class="price"><?php echo $price; ?></span>
+            <?php } //end if price ?>
+        </div>
+        <?php the_post_thumbnail(); ?>
+    </article>
 
-<?php 
-        endwhile; 
-    endif;
-endforeach;
-?>
+    <?php 
+            endwhile; 
+        endif;
+    endforeach;
+    ?>
+</div>
 </main>
 <?php get_footer() ?>
